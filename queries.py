0c7d63768a6976a762c5be3bb7c0dea02d4e9097
AndrasKovacs84 <ka841001@gmail.com>
2017-05-17 11:38:08 +0200

    queries.py function names added

--- queries.py
+++ queries.py
@@ -2,11 +2,12 @@ from connect import connect_to_sql
 
 
 @connect_to_sql
-def select_statement(query):
+def sql_select(query):
     '''Displays the query chosen by the user. Parameter is a dictionary of the relevant properties
     for a simple query. Returns the results of the query. data[0] = list of column names as header,
     data[1] = list of lists, where each nested list represents a row of data.'''
-    data = []
+    data = {'header': [],
+            'result_set':[]}
     cursor.execute("""
                    SELECT DISTINCT {1}
                    FROM {0}
@@ -15,8 +16,23 @@ def select_statement(query):
                    """.format(query['table'], ", ".join(query['columns']), query['filter'], query['order_by']))
     column_names = [desc[0] for desc in cursor.description]
     rows = cursor.fetchall()
-    data.append(column_names)
-    data.append(rows)
+
+    data['header'] = column_names
+    data['result_set'] = rows
     return data
 
 
+@connect_to_sql
+def sql_insert():
+    ''''''
+    pass
+
+
+@connect_to_sql
+def sql_update():
+    pass
+
+
+@connect_to_sql
+def sql_delete():
+    pass
