a69244ad08150d48e140c232df83c01703bec5bc
AndrasKovacs84 <ka841001@gmail.com>
2017-05-17 11:12:15 +0200

    File structure initialized

---
+++ queries.py
@@ -0,0 +1,22 @@
+from connect import 
+
+
+@
+def select_statement(query):
+    '''Displays the query chosen by the user. Parameter is a dictionary of the relevant properties
+    for a simple query. Returns the results of the query. data[0] = list of column names as header,
+    data[1] = list of lists, where each nested list represents a row of data.'''
+    data = []
+    cursor.execute("""
+                   SELECT DISTINCT {1}
+                   FROM {0}
+                   WHERE {2}
+                   ORDER BY {3};
+                   """.format(query['table'], ", ".join(query['columns']), query['filter'], query['order_by']))
+    column_names = [desc[0] for desc in cursor.description]
+    rows = cursor.fetchall()
+    data.append(column_names)
+    data.append(rows)
+    return data
+
+
