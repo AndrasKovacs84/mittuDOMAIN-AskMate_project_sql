7061e72fb0f3cf51a4f2ffaa21cdcde8333399de
András Kovács <ka841001@gmail.com>
2017-06-01 21:53:30 +0200

    multi_table_queries rewritten for added security (some functions remain with .format, their variables are not from user input)

--- data_access/multi_table_queries.py
+++ data_access/multi_table_queries.py
@@ -31,20 +31,21 @@ def list_questions(cursor, sort='submission_time DESC'):
 def answers_to_question(cursor, question_id):
     data = {'header': [],
             'result_set': []}
-    cursor.execute("""
-                   SELECT
-                   answer.id AS "Id",
-                   user_mates.user_mates_name AS "Author",
-                   answer.submission_time AS "Submission time",
-                   answer.vote_number AS "Vote number",
-                   answer.message AS "Message",
-                   answer.image AS "Image"
-                   FROM answer
-                   INNER JOIN user_mates
-                   ON answer.user_mates_id = user_mates.id
-                   WHERE answer.question_id = {0}
-                   ORDER BY answer.submission_time
-                   """.format(question_id))
+    query = """
+            SELECT
+            answer.id AS "Id",
+            user_mates.user_mates_name AS "Author",
+            answer.submission_time AS "Submission time",
+            answer.vote_number AS "Vote number",
+            answer.message AS "Message",
+            answer.image AS "Image"
+            FROM answer
+            INNER JOIN user_mates
+            ON answer.user_mates_id = user_mates.id
+            WHERE answer.question_id = %s
+            ORDER BY answer.submission_time;
+            """
+    cursor.execute(query, (question_id,))
     column_names = [desc[0] for desc in cursor.description]
     rows = cursor.fetchall()
     data['header'] = column_names
@@ -56,18 +57,19 @@ def answers_to_question(cursor, question_id):
 
 @connect_to_sql
 def gather_comments_for_answer(cursor, answer_id):
-    cursor.execute("""
-                   SELECT
-                   comment.id AS "Id",
-                   user_mates.user_mates_name AS "Author",
-                   comment.message AS "Message",
-                   comment.submission_time AS "Submission time"
-                   FROM comment
-                   INNER JOIN user_mates
-                   ON comment.user_mates_id = user_mates.id
-                   WHERE answer_id = {0}
-                   ORDER BY comment.submission_time
-                   """.format(answer_id))
+    query = """
+            SELECT
+            comment.id AS "Id",
+            user_mates.user_mates_name AS "Author",
+            comment.message AS "Message",
+            comment.submission_time AS "Submission time"
+            FROM comment
+            INNER JOIN user_mates
+            ON comment.user_mates_id = user_mates.id
+            WHERE answer_id = %s
+            ORDER BY comment.submission_time
+            """
+    cursor.execute(query, (answer_id,))
     comments = cursor.fetchall()
     return comments
 
@@ -76,18 +78,19 @@ def gather_comments_for_answer(cursor, answer_id):
 def gather_question_comments(cursor, question_id):
     data = {'header': [],
             'result_set': []}
-    cursor.execute("""
-                   SELECT
-                   comment.id AS "Id",
-                   user_mates.user_mates_name AS "Author",
-                   comment.message AS "Message",
-                   comment.submission_time AS "Submission time"
-                   FROM comment
-                   INNER JOIN user_mates
-                   ON comment.user_mates_id = user_mates.id
-                   WHERE question_id = {0}
-                   ORDER BY comment.submission_time
-                   """.format(question_id))
+    query = """
+            SELECT
+            comment.id AS "Id",
+            user_mates.user_mates_name AS "Author",
+            comment.message AS "Message",
+            comment.submission_time AS "Submission time"
+            FROM comment
+            INNER JOIN user_mates
+            ON comment.user_mates_id = user_mates.id
+            WHERE question_id = %s
+            ORDER BY comment.submission_time
+            """
+    cursor.execute(query, (question_id,))
     column_names = [desc[0] for desc in cursor.description]
     rows = cursor.fetchall()
     data['header'] = column_names
@@ -127,30 +130,33 @@ def get_user_activities_of_id(cursor, user_id):
     user_activities = {'questions': [],
                        'answers': [],
                        'comments': []}
-    cursor.execute("""
-                   SELECT id, title, message
-                   FROM question
-                   WHERE user_mates_id={0}
-                   """.format(user_id))
+    query = """
+            SELECT id, title, message
+            FROM question
+            WHERE user_mates_id=%s
+            """
+    cursor.execute(query, (user_id,))
     rows = cursor.fetchall()
     for row in rows:
         user_activities['questions'].append({'id': row[0],
                                              'title': row[1],
                                              'message': row[2]})
-    cursor.execute("""
-                   SELECT question_id, message
-                   FROM answer
-                   WHERE user_mates_id={0}
-                   """.format(user_id))
+    query = """
+            SELECT question_id, message
+            FROM answer
+            WHERE user_mates_id=%s
+            """
+    cursor.execute(query, (user_id,))
     rows = cursor.fetchall()
     for row in rows:
         user_activities['answers'].append({'question_id': row[0],
                                            'message': row[1]})
-    cursor.execute("""
-                   SELECT question_id, answer_id, message
-                   FROM comment
-                   WHERE user_mates_id={0}
-                   """.format(user_id))
+    query = """
+            SELECT question_id, answer_id, message
+            FROM comment
+            WHERE user_mates_id=%s
+            """
+    cursor.execute(query, (user_id,))
     rows = cursor.fetchall()
     for row in rows:
         user_activities['comments'].append({'question_id': row[0],
@@ -160,9 +166,9 @@ def get_user_activities_of_id(cursor, user_id):
 
 
 @connect_to_sql
-def update_vote_nr(cursor, table, id, amount):
+def update_vote_nr(cursor, table, item_id, amount):
     cursor.execute("""
                    UPDATE {0}
                    SET vote_number = vote_number{1}
                    WHERE id={2}
-                   """.format(table, amount, id))
+                   """.format(table, amount, item_id))
