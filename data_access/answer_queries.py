757437e4762528143bef52a53a125f5af3af9389
András Kovács <ka841001@gmail.com>
2017-06-01 21:20:40 +0200

    answer_queries.py rewritten for increased security.

--- data_access/answer_queries.py
+++ data_access/answer_queries.py
@@ -3,17 +3,18 @@ from .server_connection.connect import connect_to_sql
 
 @connect_to_sql
 def answer_details(cursor, answer_id):
-    cursor.execute("""
-                   SELECT
-                   id AS "Id",
-                   submission_time AS "Submission time",
-                   vote_number AS "Vote number",
-                   question_id AS "Question id",
-                   message AS "Message",
-                   image AS "Image"
-                   FROM answer
-                   WHERE id={0}
-                   """.format(answer_id))
+    query = """
+            SELECT
+            id AS "Id",
+            submission_time AS "Submission time",
+            vote_number AS "Vote number",
+            question_id AS "Question id",
+            message AS "Message",
+            image AS "Image"
+            FROM answer
+            WHERE id=%s
+            """
+    cursor.execute(query, (answer_id,))
     result = cursor.fetchall()
     answer = {'id': result[0][0],
               'submission_time': result[0][1],
@@ -26,12 +27,14 @@ def answer_details(cursor, answer_id):
 
 @connect_to_sql
 def delete_answer(cursor, question_id,):
-    cursor.execute(""" DELETE FROM answer WHERE question_id = '{0}'; """.format(question_id))
+    statement = """ DELETE FROM answer WHERE question_id = %s; """
+    cursor.execute(statement, (question_id,))
 
 
 @connect_to_sql
 def insert_answer(cursor, init_answer, question_id):
-    cursor.execute("""
-                   INSERT INTO answer (submission_time, vote_number, question_id, message, user_mates_id)
-                   VALUES ('{0}', '{1}', (SELECT id FROM question WHERE id='{2}'),'{3}', {4});
-                   """.format(init_answer[0], init_answer[1], question_id, init_answer[2], init_answer[3]))
+    statement = """
+                INSERT INTO answer (submission_time, vote_number, question_id, message, user_mates_id)
+                VALUES (%s, %s, (SELECT id FROM question WHERE id=%s), %s, %s);
+                """
+    cursor.execute(statement, (init_answer[0], init_answer[1], question_id, init_answer[2], init_answer[3]))
